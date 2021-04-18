0ce019cd2e980de6635b4d04c9ab9817113ed3d2
Diego Gómez <diego.gomez@digitalonus.com>
2021-04-18 09:30:53 -0500

    Exercise 1

---
+++ Practicas/p1/p1.py
@@ -0,0 +1,14 @@
+age = int(input('Ingrese edad\n'))
+
+
+if age > 60:
+    print('Tercera edad')
+    
+elif age > 18 & age < 60:
+    print('Adulto')
+    
+elif age > 15 & age < 18:
+    print('Joven')
+    
+elif age > 10 & age < 15:
+    print('Adolescente')
